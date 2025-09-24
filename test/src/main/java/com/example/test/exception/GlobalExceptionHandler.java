e9ade7f700c0c05cf4e4a22bec9e548423d63f0d
ahlam <halimaazioui17@gmail.com>
2025-09-25 00:48:03 +0200

    personalisation de page non trouvée + gestion des exceptions avec ControllerAdvice

---
+++ test/src/main/java/com/example/test/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.test.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Gestion des URLs introuvables → page HTML 404
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handle404() {
+        return "error/404"; // le fichier 404.html dans templates
+    }
+
+
+}
